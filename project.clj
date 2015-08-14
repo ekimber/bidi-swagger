44a0fa79fdb8581e83e426cfaf9ea7a2bc55f1c1
Edward Kimber <edward.kimber@gmail.com>
2015-08-14 15:43:04 +0100

    project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bidi-swagger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small Clojure library for creating swagger docs from bidi routes."
+  :url "https://github.com/ekimber/bidi-swagger"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
